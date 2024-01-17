eeacdedd6cb48f898bec3daa5e4a3370c388df74
eyob_worku <eyobworku62@gmail.com>
2024-01-17 13:57:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print(update)
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("6628499584:AAGV74jBFEELTbqFgTzfT_jMyJnKxo5zgZQ").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling(allowed_updates=Update.ALL_TYPES)
