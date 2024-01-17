d8362749af4ac26a4bb7d4ba0c7d2f715cb13307
eyob_worku <eyobworku62@gmail.com>
2024-01-17 14:50:41 +0300

    add eyu command

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+async def eyu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print(update)
+    await update.message.reply_text(f'Hello {update.effective_user.username}')
 
 app = ApplicationBuilder().token("6628499584:AAGV74jBFEELTbqFgTzfT_jMyJnKxo5zgZQ").build()
 
 app.add_handler(CommandHandler("hello", hello))
+
+app.add_handler(CommandHandler("eyu", eyu))
 
 # app.run_polling(allowed_updates=Update.ALL_TYPES)
 app.run_webhook(
