2585463bd278ce1673509b5350057a0c2cff150e
Eyob Worku <eyobworku62@gmail.com>
2024-05-07 13:26:04 +0000

    Final convertion bot

--- clod.py
+++ clod.py
@@ -1,40 +1,44 @@
 import cloudconvert
-import base64
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-api_key = os.environ.get("API_KEY")
+api_key = os.environ.get("API_KEY2")
 
 cloudconvert.configure(api_key=api_key, sandbox=False)
+link = "https://api.telegram.org/file/bot6834520500:AAFt-zeDAnljl9dWQn2P-_zP2QglFF7eGvE/documents/file_7.epub"
 
-f = open("Notes_from_Underground.epub", "rb")
-encoded_string = base64.b64encode(f.read()).decode('utf-8')
-#decf = encoded_string.decode('utf-8')
 
-job = cloudconvert.Job.create(
-    payload={
-        "tasks": {
-            'import-my-file': {
-                "file": encoded_string,
-                "filename": "Notes_from_Underground.epub",
-            },
-            "convert-my-file": {
-                "operation": "convert",
-                "input": "import-my-file",
-                "output_format": "pdf",
-            },
-            "export-my-file": {"operation": "export/url", "input": "convert-my-file"},
+def convertPdf(link, file_name):
+    job = cloudconvert.Job.create(
+        payload={
+            "tasks": {
+                "import-my-file": {"operation": "import/url", "url": link},
+                "convert-my-file": {
+                    "operation": "convert",
+                    "input": "import-my-file",
+                    'filename':file_name,
+                    "output_format": "pdf",
+                    "some_other_option": "value",
+                },
+                "export-my-file": {
+                    "operation": "export/url",
+                    "input": "convert-my-file",
+                },
+            }
         }
-    }
-)
+    )
 
-job = cloudconvert.Job.wait(id=job["id"])
+    job = cloudconvert.Job.wait(id=job["id"])
 
-for task in job["tasks"]:
-    if task.get("name") == "export-it" and task.get("status") == "finished":
-        export_task = task
+    for task in job["tasks"]:
+        if task.get("name") == "export-my-file" and task.get("status") == "finished":
+            export_task = task
 
-file = export_task.get("result").get("files")[0]
-res = cloudconvert.download(filename=file["filename"], url=file["url"])
-print(type(res))
+    file = export_task.get("result").get("files")[0]
+    return file["url"]
+    # cloudconvert.download(filename=file_name, url=file["url"])
+    # print(file["filename"])
+
+
+# convertPdf(link,'eyub.pdf')
--- main.py
+++ main.py
@@ -1,30 +1,55 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder,ApplicationHandlerStop, CommandHandler, ContextTypes,ConversationHandler,MessageHandler,filters
+from telegram.ext import (
+    ApplicationBuilder,
+    ApplicationHandlerStop,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+from clod import convertPdf
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
-bot_token = os.environ.get('BOT_TOKEN')
+bot_token = os.environ.get("BOT_TOKEN")
+
 
 async def pre_process(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     context.user_data["step"] = context.user_data.get("step", 0) + 1
-    print(context.user_data,update.effective_user.full_name)    
+    print(context.user_data, update.effective_user.full_name)
     return None
 
+
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await pre_process(update,context)
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await pre_process(update, context)
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
+
 
 async def eyu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await pre_process(update,context)
-    await update.message.reply_text(f'Hello {update.effective_user.username}')
+    await pre_process(update, context)
+    await update.message.reply_text(f"Hello {update.effective_user.username}")
+
+
+async def myFile(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    file_name = update.message.document.file_name.split('.')[0] + '.pdf'
+    file_id = update.message.document.file_id
+    new_file = await update.message.document.get_file()
+    conUrl = convertPdf(new_file.file_path, file_name)
+    await update.message.from_user.send_document(document=conUrl)
+    await update.message.reply_text(f"Your file converted {update.effective_user.username}")
+
 
 app = ApplicationBuilder().token(bot_token).build()
 
 # app.add_handler(MessageHandler(None, pre_process))
 
-app.add_handler(CommandHandler(["hello",'start'], hello))
+app.add_handler(CommandHandler(["hello", "start"], hello))
+
+app.add_handler(MessageHandler(filters.Document.ALL, myFile))
 
 app.add_handler(CommandHandler("eyu", eyu))
 
-app.run_polling(allowed_updates=Update.ALL_TYPES)
-# app.run_webhook(listen='0.0.0.0',port=443,webhook_url='https://pptbot.onrender.com/')
+#app.run_polling(allowed_updates=Update.ALL_TYPES)
+app.run_webhook(listen='0.0.0.0',port=443,webhook_url='https://epubpdf.onrender.com/')
