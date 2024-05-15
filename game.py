3eb5558851fabf4dc7ec31bf00775fc165bdf484
Eyob Worku <eyobworku62@gmail.com>
2024-05-15 07:57:49 +0000

    basic bot file

---
+++ game.py
@@ -0,0 +1,33 @@
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    ApplicationHandlerStop,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+bot_token = os.environ.get("BOT_TOKENPOLL")
+
+
+async def pre_process(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.user_data["step"] = context.user_data.get("step", 0) + 1
+    print(context.user_data, update.effective_user.full_name)
+    return None
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await pre_process(update, context)
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
+
+app = ApplicationBuilder().token(bot_token).build()
+
+app.add_handler(CommandHandler(["hello", "start"], hello))
+
+app.run_polling(allowed_updates=Update.ALL_TYPES)
+# app.run_webhook(listen='0.0.0.0',port=443,webhook_url='https://epubpdf.onrender.com/')
