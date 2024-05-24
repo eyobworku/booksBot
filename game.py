e75cb5bd5d1a19691e56ff93bc3bbf0e8bb419b2
Eyob Worku <eyobworku62@gmail.com>
2024-05-24 12:58:52 +0000

    Leet and CF

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ from telegram.ext import (
     ApplicationHandlerStop,
     CommandHandler,
     ContextTypes,
-    ConversationHandler,
+    CallbackQueryHandler,
     MessageHandler,
     filters,
 )
@@ -25,9 +25,17 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await pre_process(update, context)
     await update.message.reply_text(f"Hello {update.effective_user.first_name}")
 
+async def tts_callback(update, context):
+    query = update.callback_query
+    bot = context.bot
+    # print(query.game_short_name)
+    await bot.answerCallbackQuery(query.id,url='https://tbot.xyz/lumber/')
+
 app = ApplicationBuilder().token(bot_token).build()
 
 app.add_handler(CommandHandler(["hello", "start"], hello))
+
+app.add_handler(CallbackQueryHandler(tts_callback))
 
 app.run_polling(allowed_updates=Update.ALL_TYPES)
 # app.run_webhook(listen='0.0.0.0',port=443,webhook_url='https://epubpdf.onrender.com/')
